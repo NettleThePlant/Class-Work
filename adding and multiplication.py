5df7b502c8766f2a69f11dc9fbf860696cf51bd0
Abi <21aduda.acc@lwetb.ie>
2025-09-02 14:26:00 +0100

    Add files via upload

---
+++ adding and multiplication.py
@@ -0,0 +1,18 @@
+#Author: Abigail
+#Date:September 1st
+#Description: adding and multiplication
+
+int1 = int(input('Please enter your first number: '))
+int2 = int(input('Please enter your second number: '))
+
+print(int1+int2)
+sumOfAddition = (int1+int2)
+
+print('The sum of ',int1, 'and ', int2, 'is ', sumOfAddition)
+
+int3 = int(input('Please enter your third number: '))
+
+sumOfMultiplication = (int3 * sumOfAddition)
+
+print('The answer is: ', sumOfMultiplication)
+
