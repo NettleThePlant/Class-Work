c8e15c31886bd17e79ed56562b05976e53034c48
Abi <21aduda.acc@lwetb.ie>
2025-09-02 14:33:15 +0100

    Add files via upload

---
+++ class work 2nd sep.py
@@ -0,0 +1,7 @@
+#Author:Abigail
+#Date: Septer 2nd
+#Description: Calculating the area of a circle with a given radius
+
+radius = float(input('Please enter your radius: '))
+area = float(3.141*radius*radius)
+print('The area of your cicle is ', area)
