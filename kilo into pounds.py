c3ab2ae961454f2a481377911cae30cb4cdd656e
Abi <21aduda.acc@lwetb.ie>
2025-09-02 14:22:31 +0100

    Add files via upload

---
+++ kilo into pounds.py
@@ -0,0 +1,9 @@
+#Author: Abigail
+#Date:1st September
+#Description: calculating the amount of pounds in a certain amount of kilograms
+
+numberOfKilograms = float(input('Please enter your amount of kilograms: '))
+
+convertedPounds = float((numberOfKilograms*2.204))
+
+print('There are', convertedPounds, ' pounds in ',numberOfKilograms, 'kilograms')
