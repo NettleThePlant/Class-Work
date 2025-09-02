d3f58c8a44eff5996e4c8ba81a77b09ed3920d6a
Abi <21aduda.acc@lwetb.ie>
2025-09-02 14:17:26 +0100

    Add files via upload

---
+++ Next year age.py
@@ -0,0 +1,10 @@
+#Author: Abigail
+#Date:1st September
+#Description: calculating next years age
+
+user_nameQ6 = input(' Please enter your full name: ')
+user_age = int(input('Please enter your age: '))
+                    
+future_user_age = (user_age + 1)
+
+print('Hello', user_nameQ6, 'Next year, you will be', future_user_age, ' years old!')
