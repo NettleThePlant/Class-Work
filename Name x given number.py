d1ccc45d12f222d8c7e24b76b22f8b0818275248
Abi <21aduda.acc@lwetb.ie>
2025-09-08 21:40:48 +0100

    Add files via upload

---
+++ Name x given number.py
@@ -0,0 +1,10 @@
+'''
+Author: Abigail
+Date: 08/09/2025
+Description: displaying a given name and number three times
+'''
+
+name_question = input('Please enter your name: ')
+number_question = int(input('Please enter your number: '))
+
+print((name_question + ' ')*number_question)
