ffb52b93e47e3fb5669e689b6fe747c482e32b05
Abi <21aduda.acc@lwetb.ie>
2025-09-08 21:41:33 +0100

    Update Name x given number.py

--- Name x given number.py
+++ Name x given number.py
@@ -1,10 +1,11 @@
 '''
 Author: Abigail
 Date: 08/09/2025
-Description: displaying a given name and number three times
+Description: displaying the users name a givennumber of times
 '''
 
 name_question = input('Please enter your name: ')
 number_question = int(input('Please enter your number: '))
+
 
 print((name_question + ' ')*number_question)
