d412e67d6d34d641ca5afa3fe0332405b5da60f0
Abi <21aduda.acc@lwetb.ie>
2025-09-11 09:34:32 +0100

    Add files via upload

---
+++ programming two/Displaying the users name on a different line for each letter.py
@@ -0,0 +1,9 @@
+'''
+Author: Abigail
+Date: 08/09/2025
+Description: Displaying the users name on a different line for each letter
+'''
+name = input('Please enter your name: ')
+
+for i in name:
+    print(i)
