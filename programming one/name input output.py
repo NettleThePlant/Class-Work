fd82b07b4d45d4569e7426e153009db122f93fb0
cmeehan-acc <cmeehan.acc@lwetb.ie>
2025-09-17 12:36:22 +0100

    Feedback 1

---
+++ programming one/name input output.py
@@ -0,0 +1,15 @@
+#Author:Abigail Duda
+#Date: September 1st 2025
+#Description: Program to request user name as input and return same with a greeting
+
+user_name = input( 'Please enter your name: ')
+user_surname = input('Please enter your surname: ')
+
+
+greeting = 'Hello'
+
+
+print(greeting, user_name, user_surname)
+
+
+
