37005962bd49fbe6942d8559609c5fcd4d8a4984
Abi <21aduda.acc@lwetb.ie>
2025-09-19 09:41:53 +0100

    Add files via upload

---
+++ flowchart work/odd or even.py
@@ -0,0 +1,10 @@
+#Author:Abigail Duda
+#Date: 19/9/2025
+#Description:checking if a number is odd or even
+
+num = int(input('Enter your number: '))
+
+if num%2 == 0:
+    print(num,'is an even number')
+else:
+    print(num,'is an odd number')
