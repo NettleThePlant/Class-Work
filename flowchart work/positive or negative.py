ba9ceed1a9e8be34474c62b3a71f2b6499a47906
Abi <21aduda.acc@lwetb.ie>
2025-09-19 09:39:21 +0100

    Add files via upload

---
+++ flowchart work/positive or negative.py
@@ -0,0 +1,10 @@
+#Author:Abigail Duda
+#Date: 19/9/2025
+#Description:checking is a number is positive of negative
+
+num = int(input('please enter your number: '))
+
+if num>=0:
+    print(num, ' is positive')
+else:
+    print(num, ' is negative')
