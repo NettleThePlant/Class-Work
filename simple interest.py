8a5f9407287e764e4f19ee6baf036ad45cea2c47
Abi <21aduda.acc@lwetb.ie>
2025-09-19 09:38:23 +0100

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,12 @@
+#Author:Abigail Duda
+#Date: 19/9/2025
+#Description: Calculating the simple interest
+
+prin = float(input('Enter your principal: '))
+rate = float(input('Enter your interest rate in terms of decimal percentage (per year): '))
+time = float(input('Please enter your time in years: '))
+
+interest = (prin*rate*time)
+float(interest) 
+
+print('Your total interest is ', interest)
