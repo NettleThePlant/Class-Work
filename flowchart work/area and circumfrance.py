9bc311059070277989624b99b561f1a55d881c90
Abi <21aduda.acc@lwetb.ie>
2025-09-19 09:39:44 +0100

    Add files via upload

---
+++ flowchart work/area and circumfrance.py
@@ -0,0 +1,11 @@
+#Author:Abigail Duda
+#Date: 19/9/2025
+#Description: Calculating the area and circumfrance of a circle with a given radius
+
+radius = float(input('Please enter your radius in cm: '))
+
+area = float(3.141*radius*radius)
+circum = float(2*3.141*radius)
+
+print('The area in cm is ', area)
+print('The circumfrance in cm is ', circum)
