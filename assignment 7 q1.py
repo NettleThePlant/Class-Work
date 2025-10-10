1a6117f3bb4a91eb0db6c66c1d9e5b61bfb50cac
Abi <21aduda.acc@lwetb.ie>
2025-10-10 09:09:25 +0100

    Add files via upload

---
+++ assignment 7 q1.py
@@ -0,0 +1,19 @@
+'''
+Author: Abigail
+Date: 9/10/15
+Description: checking if an given number is an armstriong number  
+'''
+
+total = 0
+x = input('Enter your number: ')
+
+num_digit = len(x)
+
+for digit in x:
+    total = total + int(digit) ** num_digit
+    
+if total == int(x):
+    print('Armstrong number')
+
+else:
+    print('Not armstrong number')
