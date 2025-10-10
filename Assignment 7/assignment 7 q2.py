7965e7ee0b4230b918ea145944ba7600e215c810
Abi <21aduda.acc@lwetb.ie>
2025-10-10 09:34:56 +0100

    Update assignment 7 q2.py

--- Assignment 7/assignment 7 q2.py
+++ Assignment 7/assignment 7 q2.py
@@ -1,6 +1,6 @@
 '''
 Author: Abigail
-Date: 10/10/15
+Date: 10/10/25
 Description: Entering numbers until the user enters 0 and printing the count of negative and positive numbers
 '''
 
@@ -16,4 +16,5 @@ while n != 0:
     count+=1
     
     if n == 0:
+
         print('The total amount of numbers entered before zero was', count)
