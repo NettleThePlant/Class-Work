5121277e83002fdb428a376ea6b5f06429399394
Abi <21aduda.acc@lwetb.ie>
2026-01-22 09:44:08 +0000

    Add files via upload

---
+++ List Questions from Book/Question 7 (a).py
@@ -0,0 +1,11 @@
+'''
+Abigail
+22/1/26
+Question 7 part (a)
+'''
+
+lst = []
+for i in range(0,50):
+    lst.append(i)
+    print(lst)
+
---
+++ List Questions from Book/Question 7 (b).py
@@ -0,0 +1,11 @@
+'''
+Abigail
+22/1/26
+Question 7 part (b)
+'''
+
+lst = []
+for i in range(1,51):
+    i =i**2
+    lst.append(i)
+    print(lst)
